f1b2a815375274c823711873873d52cd32f2ad40
Md. Ruhul AMin <ruhulmath08@gmail.com>
2019-03-03 00:09:20 +0600

    6. Spring Bean Scopes and Lifecycle

---
+++ 6. Spring Bean Scopes and Lifecycle/6-spring-bean-scope-and-lifecycle/src/com/odduu/ruhul/BeanScopeDemoApp.java
@@ -0,0 +1,28 @@
+package com.odduu.ruhul;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"beanLifeCycle-applicationContext.xml");
+
+		// retrieve bean from container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+
+		// print out the result
+		System.out.println("Pointing to the same object: " + result);
+		System.out.println("Memory location for theCoach: " + theCoach);
+		System.out.println("Memory location for alphaCoach: " + alphaCoach);
+
+		// close the context
+		context.close();
+	}
+}
---
+++ 6. Spring Bean Scopes and Lifecycle/6-spring-bean-scope-and-lifecycle/src/com/odduu/ruhul/TrackCoach.java
@@ -0,0 +1,30 @@
+package com.odduu.ruhul;
+
+public class TrackCoach implements Coach {
+
+	private FortuneService fortuneService;
+
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just Do It: " + fortuneService.getFortune();
+	}
+
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff()");
+	}
+
+	// add an destroy method
+	public void doMyCleaneupStuff() {
+		System.out.println("TrackCoach: inside method doMyCleaneupStuff()");
+	}
+}
