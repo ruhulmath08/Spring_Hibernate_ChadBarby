b356d6d6c7f5d1829e1c987658027916fe90f74d
unknown <ruhulmath08@gmail.com>
2019-03-11 16:34:29 +0600

    10. Spring Configuration with Java Code

---
+++ 10. Spring Configuration with Java Code/10-spring-configuration-java-code/src/com/ruhul/odduu/javaconfiguration/FortuneService.java
@@ -0,0 +1,6 @@
+package com.ruhul.odduu.javaconfiguration;
+
+public interface FortuneService {
+
+	public String getFortune();
+}
---
+++ 10. Spring Configuration with Java Code/10-spring-configuration-java-code/src/com/ruhul/odduu/javaconfiguration/SportConfig.java
@@ -0,0 +1,10 @@
+package com.ruhul.odduu.javaconfiguration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ruhul.odduu.javaconfiguration")
+public class SportConfig {
+
+}
