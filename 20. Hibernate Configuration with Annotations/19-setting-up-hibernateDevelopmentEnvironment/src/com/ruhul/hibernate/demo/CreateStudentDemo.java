81bd03a912fd415488e4f9ec006c5c7f7946ab3c
Md. Ruhul AMin <ruhulmath08@gmail.com>
2019-03-30 01:49:01 +0600

    Hibernate Config and CRUD

---
+++ 20. Hibernate Configuration with Annotations/19-setting-up-hibernateDevelopmentEnvironment/src/com/ruhul/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,40 @@
+package com.ruhul.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ruhul.hibernate.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create a session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// create a Student object
+			System.out.println("Creating new student object...");
+			Student tempStudent = new Student("Md. Ruhul Amin", "Ruhul", "ruhul@gmail.com");
+
+			// start a transaction
+			session.beginTransaction();
+
+			// save the student object
+			System.out.println("Saving the student...");
+			session.save(tempStudent);
+
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!!");
+		} finally {
+			factory.close();
+		}
+	}
+
+}
