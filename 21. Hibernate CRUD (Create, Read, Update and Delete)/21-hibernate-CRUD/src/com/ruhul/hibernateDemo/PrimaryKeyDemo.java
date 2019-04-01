0dbee744dd0a11ccbd96e149f59385c2c6df1aa5
Md. Ruhul AMin <ruhulmath08@gmail.com>
2019-04-01 23:34:17 +0600

    01-04-2019

--- 21. Hibernate CRUD (Create, Read, Update and Delete)/21-hibernate-CRUD/src/com/ruhul/hibernateDemo/PrimaryKeyDemo.java
+++ 21. Hibernate CRUD (Create, Read, Update and Delete)/21-hibernate-CRUD/src/com/ruhul/hibernateDemo/PrimaryKeyDemo.java
@@ -6,23 +6,26 @@ import org.hibernate.cfg.Configuration;
 
 import com.ruhul.hibernateCRUD.entity.Student;
 
-public class CreateStudentDemo {
+public class PrimaryKeyDemo {
 	public static void main(String[] args) {
 
-		//create session factory
+		// create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
 								.addAnnotatedClass(Student.class)
 								.buildSessionFactory();
 
-		//create session
+		// create session
 		Session session = factory.getCurrentSession();
 
 		try {
 
 			// create a student object
 			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Md. Rezaul Islam", "Reza", "reza@gmail.com");
+			Student tempStudent = new Student("Md. Ruhul Amin", "Ruhul", "ruhul@gmail.com");
+			Student tempStudent1 = new Student("Md. Rezaul Islam", "Reza", "reza@gmail.com");
+			Student tempStudent2 = new Student("Md. Ariful Islam", "Arif", "arif@gmail.com");
+			Student tempStudent3 = new Student("Md. Rafiul Islam", "Rafi", "rafi@gmail.com");
 
 			// start transaction
 			session.beginTransaction();
@@ -30,6 +33,9 @@ public class CreateStudentDemo {
 			// save the student
 			System.out.println("Save the student...");
 			session.save(tempStudent);
+			session.save(tempStudent1);
+			session.save(tempStudent2);
+			session.save(tempStudent3);
 
 			// commit the transaction
 			session.getTransaction().commit();
