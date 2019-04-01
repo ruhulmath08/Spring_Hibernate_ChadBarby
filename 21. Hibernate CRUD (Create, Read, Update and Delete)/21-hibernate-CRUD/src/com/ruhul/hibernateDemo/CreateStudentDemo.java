57645543562e82ea7c4effef998714a93a334995
unknown <ruhulmath08@gmail.com>
2019-04-01 17:01:09 +0600

    up to lecture 21

---
+++ 21. Hibernate CRUD (Create, Read, Update and Delete)/21-hibernate-CRUD/src/com/ruhul/hibernateDemo/CreateStudentDemo.java
@@ -0,0 +1,41 @@
+package com.ruhul.hibernateDemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ruhul.hibernateCRUD.entity.Student;
+
+public class CreateStudentDemo {
+	public static void main(String[] args) {
+
+		//create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+
+		//create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student tempStudent = new Student("Md. Rezaul Islam", "Reza", "reza@gmail.com");
+
+			// start transaction
+			session.beginTransaction();
+
+			// save the student
+			System.out.println("Save the student...");
+			session.save(tempStudent);
+
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!!");
+		} finally {
+			factory.close();
+		}
+	}
+}
