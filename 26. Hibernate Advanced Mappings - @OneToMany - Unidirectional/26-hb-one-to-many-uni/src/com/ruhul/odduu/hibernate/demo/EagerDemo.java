a8e9eccaca58284400d78866701a60800b1a16f5
ruhulmath08 <ruhulmath08@gmail.com>
2019-04-25 17:24:32 +0600

    25-04-2019

---
+++ 26. Hibernate Advanced Mappings - @OneToMany - Unidirectional/26-hb-one-to-many-uni/src/com/ruhul/odduu/hibernate/demo/EagerDemo.java
@@ -0,0 +1,48 @@
+package com.ruhul.odduu.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ruhul.odduu.hibernate.entity.eager.Course;
+import com.ruhul.odduu.hibernate.entity.eager.Instructor;
+import com.ruhul.odduu.hibernate.entity.eager.InstructorDetail;
+
+public class EagerDemo {
+
+	public static void main(String args[]) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start transaction
+			session.beginTransaction();
+
+			// get the instructor from DB
+			int theID = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theID);
+			System.out.println("com.ruhul: Instructor: "+tempInstructor);
+			
+			//get courses for the instructor
+			System.out.println("com.ruhul: Courses: "+tempInstructor.getCourses());
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("com.ruhul: Done!!!");
+		} finally {
+			// add clean up code
+			session.close();
+			factory.close();
+		}
+	}
+}
