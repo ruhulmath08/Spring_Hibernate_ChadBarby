e94b9713faac11a0881342cac2012eb84975a39b
Md. Ruhul AMin <ruhulmath08@gmail.com>
2019-05-06 00:11:04 +0600

    06-05-2019

--- 28-33. Build a Database Web App - SpringMVC and Hibernate/28-33-SpringMVC-Hibernate-CRUD/src/com/odduu/ruhul/testdb/TestDBServlet.java
+++ 28-33. Build a Database Web App - SpringMVC and Hibernate/28-33-SpringMVC-Hibernate-CRUD/src/com/odduu/ruhul/testdb/TestDBServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TestDbServlet
  */
-@WebServlet("/TestDbServlet")
+@WebServlet("/TestDBServlet")
 public class TestDBServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
