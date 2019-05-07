84d8d6dd1558121d6198870ca01ad4220ab36798
ruhulmath08 <ruhulmath08@gmail.com>
2019-05-07 15:52:50 +0600

    07-05-2019

---
+++ 28-33. Build a Database Web App - SpringMVC and Hibernate/spring-mvc-crud-demo/src/com/odduu/ruhul/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.odduu.ruhul.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.odduu.ruhul.dao.CustomerDAO;
+import com.odduu.ruhul.entity.Customer;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	// need to inject the customerDAO
+	@Autowired
+	private CustomerDAO customerDAO;
+
+	@RequestMapping("/list")
+	public String listCustomer(Model theModel) {
+
+		// get customer from the DAO
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customer to the model
+		// theModel.addAttribute("customers -> name", theModel -> value);
+		theModel.addAttribute("customers", theCustomers);
+		
+		return "list-customer";
+	}
+
+}
