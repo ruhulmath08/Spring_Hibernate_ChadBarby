5dd83fa2e621e1957da27d313f7b849b33c283bd
ruhulmath08 <ruhulmath08@gmail.com>
2019-05-12 15:31:18 +0600

    12-05-2019

--- 28-33. Build a Database Web App - SpringMVC and Hibernate/Code-30/spring-mvc-crud-demo/src/com/odduu/ruhul/controller/CustomerController.java
+++ 28-33. Build a Database Web App - SpringMVC and Hibernate/Code-30/spring-mvc-crud-demo/src/com/odduu/ruhul/controller/CustomerController.java
@@ -8,27 +8,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.odduu.ruhul.dao.CustomerDAO;
 import com.odduu.ruhul.entity.Customer;
+import com.odduu.ruhul.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	// need to inject the customerDAO
+	// need to inject our customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	CustomerService customerService;
 
 	@GetMapping("/list")
 	public String listCustomer(Model theModel) {
 
-		// get customer from the DAO
-		List<Customer> theCustomers = customerDAO.getCustomers();
-		
+		// get customer from the service
+		List<Customer> theCustomers = customerService.getCustomers();
+
 		// add the customer to the model
 		// theModel.addAttribute("customers -> name", theModel -> value);
 		theModel.addAttribute("customers", theCustomers);
-		
+
 		return "list-customer";
 	}
 
--- 28-33. Build a Database Web App - SpringMVC and Hibernate/Code-30/spring-mvc-crud-demo/src/com/odduu/ruhul/service/CustomerServiceImpl.java
+++ 28-33. Build a Database Web App - SpringMVC and Hibernate/Code-30/spring-mvc-crud-demo/src/com/odduu/ruhul/service/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.odduu.ruhul.dao.CustomerDAO;
 import com.odduu.ruhul.entity.Customer;
@@ -16,8 +17,9 @@ public class CustomerServiceImpl implements CustomerService {
 	private CustomerDAO customerDAO;
 	
 	@Override
+	@Transactional
 	public List<Customer> getCustomers() {
-		return null;
+		return customerDAO.getCustomers();
 	}
 
 }
