9bdb1ea6353e2dea014a400db7b9502fd945fbed
ruhulmath08 <ruhulmath08@gmail.com>
2019-06-18 16:30:17 +0600

    18-06-2019(2)

---
+++ 40. AOP JoinPoints/40-aop-read-joinpoint/src/com/ruhul/aopdemo/MainDemoApp.java
@@ -0,0 +1,44 @@
+package com.ruhul.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ruhul.aopdemo.dao.AccountDAO;
+import com.ruhul.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+		// call the business method
+		Account myAccount = new Account();
+		myAccount.setName("Ruhul");
+		myAccount.setLevel("Platinum");
+		
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+
+		// call the accountdao getter/setter method
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+
+		// close the context
+		context.close();
+	}
+
+}
---
+++ 40. AOP JoinPoints/40-aop-read-joinpoint/src/com/ruhul/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,46 @@
+package com.ruhul.aopdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.ruhul.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	// apply pointcut declaration to advice
+	@Before("com.ruhul.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+
+		System.out.println("\n====>>> Executing @Before advice on method");
+
+		// display the method signature
+		MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSignature);
+
+		// display the method arguments
+
+		// get args
+		Object[] args = theJoinPoint.getArgs();
+
+		// loop thru args
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+
+			if (tempArg instanceof Account) {
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+
+				System.out.println("Account name: " + theAccount.getName());
+				System.out.println("Account level: " + theAccount.getLevel());
+			}
+		}
+
+	}
+}
