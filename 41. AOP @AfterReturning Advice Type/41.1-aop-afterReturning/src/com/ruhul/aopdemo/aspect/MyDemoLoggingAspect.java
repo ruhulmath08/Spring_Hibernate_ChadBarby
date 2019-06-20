318bd28e45f8ef7294faa08bb37ca311bd6a4f1a
ruhulmath08 <ruhulmath08@gmail.com>
2019-06-20 16:35:31 +0600

    20-06-2019

---
+++ 41. AOP @AfterReturning Advice Type/41.1-aop-afterReturning/src/com/ruhul/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,63 @@
+package com.ruhul.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.ruhul.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	// add a new advice for @AfterReturning on the findAccounts method
+	@AfterReturning(
+			pointcut = "execution(* com.ruhul.aopdemo.dao.AccountDAO.findAccounts(..))", 
+			returning = "result")
+	public void afterReturningFindAccountAdvice(JoinPoint theJoinPoint, List<Account> result) {
+
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n======>>> Executing @AfterReturning on method: " + method);
+		
+		// print out the result of the method call
+		System.out.println("\n======>>> result is: " + result);
+	}
+
+	// apply pointcut declaration to advice
+	@Before("com.ruhul.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+
+		System.out.println("\n====>>> Executing @Before advice on method");
+
+		// display the method signature
+		MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSignature);
+
+		// display the method arguments
+
+		// get args
+		Object[] args = theJoinPoint.getArgs();
+
+		// loop thru args
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+
+			if (tempArg instanceof Account) {
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+
+				System.out.println("Account name: " + theAccount.getName());
+				System.out.println("Account level: " + theAccount.getLevel());
+			}
+		}
+
+	}
+}
---
+++ 41. AOP @AfterReturning Advice Type/41.2-aop-afterReturning-modifyReturnValue/src/com/ruhul/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,62 @@
+package com.ruhul.aopdemo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.ruhul.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	private String name;
+	private String serviceCode;
+
+	// AOP after returning
+	// add new method : findAccounts()
+	public List<Account> findAccounts() {
+		List<Account> myAccounts = new ArrayList<>();
+
+		// create simple account
+		Account temp1 = new Account("Reza", "Silver");
+		Account temp2 = new Account("Goni", "Platinum");
+		Account temp3 = new Account("Kapil", "Gold");
+
+		// add them to our accounts list
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+
+		return myAccounts;
+	}
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+	}
+
+	public boolean doWork() {
+		System.out.println(getClass() + ": doWork()");
+		return false;
+	}
+
+	public String getName() {
+		System.out.println(getClass() + ": getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+}
