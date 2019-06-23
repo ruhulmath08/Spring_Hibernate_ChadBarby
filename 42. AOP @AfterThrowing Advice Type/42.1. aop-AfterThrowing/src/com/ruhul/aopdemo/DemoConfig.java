3e17fd1b40802977068157adf0f11cfb5265bde8
ruhulmath08 <ruhulmath08@gmail.com>
2019-06-23 13:26:45 +0600

    23-06-2019 => AfterThrowing

---
+++ 42. AOP @AfterThrowing Advice Type/42.1. aop-AfterThrowing/src/com/ruhul/aopdemo/DemoConfig.java
@@ -0,0 +1,13 @@
+package com.ruhul.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.ruhul.aopdemo")
+public class DemoConfig {
+
+	
+}
---
+++ 42. AOP @AfterThrowing Advice Type/42.2. aop-AfterThrowing-add-advice/src/com/ruhul/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,43 @@
+package com.ruhul.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ruhul.aopdemo.dao.AccountDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// call method to find the accounts
+		List<Account> theAccounts = null;
+		
+		// add try-catch for @AfterThrowing advice
+		try {
+			// add a boolean flag to simulate exception
+			boolean tripWrie = true;
+			theAccounts = theAccountDAO.findAccounts(tripWrie);
+		} catch (Exception e) {
+			System.out.println("\n\nMain Program ... caught exception: " + e);
+		}
+
+		// display the accounts
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+		System.out.println("---------------------------------------");
+
+		System.out.println(theAccounts);
+
+		System.out.println("\n");
+
+		// close the context
+		context.close();
+	}
+
+}
