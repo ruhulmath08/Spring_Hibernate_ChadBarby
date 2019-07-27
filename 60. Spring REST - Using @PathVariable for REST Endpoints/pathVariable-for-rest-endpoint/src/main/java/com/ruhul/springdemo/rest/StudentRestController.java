17902e4712aa332f4e613315aa9dfad9f892b47e
ruhulmath08 <ruhulmath08@gmail.com>
2019-07-27 12:19:18 +0600

    Complete => 60. Spring REST - Using @PathVariable for REST Endpoints

--- 60. Spring REST - Using @PathVariable for REST Endpoints/pathVariable-for-rest-endpoint/src/main/java/com/ruhul/springdemo/rest/StudentRestController.java
+++ 60. Spring REST - Using @PathVariable for REST Endpoints/pathVariable-for-rest-endpoint/src/main/java/com/ruhul/springdemo/rest/StudentRestController.java
@@ -18,6 +18,19 @@ public class StudentRestController {
 
 	private List<Student> theStudents;
 
+	/*
+	 * WHY WE USR: @PostConstruct
+	 * 
+	 * because when the constructor is called, the bean is not yet initialized -
+	 * i.e. no dependencies are injected. In the @PostConstruct method the bean is
+	 * fully initialized and you can use the dependencies.
+	 * 
+	 * because this is the contract that guarantees that this method will be invoked
+	 * only once in the bean lifecycle. It may happen (though unlikely) that a bean
+	 * is instantiated multiple times by the container in its internal working, but
+	 * it guarantees that @PostConstruct will be invoked only once.
+	 */
+
 	// define @PostConstruct to load the student data ... only once
 	@PostConstruct
 	private void loadData() {
