cd942ccd9926f1522d8dac5c9a68720b33d78077
ruhulmath08 <ruhulmath08@gmail.com>
2019-09-02 13:10:08 +0600

    Complete => Spring REST - CRUD Database Project

---
+++ 64 - 68. Spring REST - CRUD Database Project/spring-crm-rest-demo/src/main/java/com/ruhul/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.ruhul.springdemo.dao;
+
+import java.util.List;
+
+import com.ruhul.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
